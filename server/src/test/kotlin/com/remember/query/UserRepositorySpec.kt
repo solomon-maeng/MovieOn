76489ef935c269279795f5a5a4222d08e0eb6010
solomon.maeng <solomon.maeng@dramancompany.com>
2022-11-08 17:54:38 +0900

    Refactor test code

--- server/src/test/kotlin/com/remember/query/UserRepositorySpec.kt
+++ server/src/test/kotlin/com/remember/query/UserRepositorySpec.kt
@@ -3,16 +3,15 @@ package com.remember.query
 import com.remember.support.RepositorySpec
 import com.remember.user.domain.User
 import com.remember.user.infrastructure.jpa.JpaUserRepository
-import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 
 @RepositorySpec
 class UserRepositorySpec(private val userRepository: JpaUserRepository) : DescribeSpec({
 
     describe("UserRepository") {
+
         it("existsByEmail 쿼리 검증") {
             userRepository.existsByUserInformationEmail("some@gmail.com").shouldBeFalse()
         }
@@ -21,20 +20,19 @@ class UserRepositorySpec(private val userRepository: JpaUserRepository) : Descri
             userRepository.existsByUserInformationUsername("kitty").shouldBeFalse()
         }
 
-        it("findByEmail 쿼리 검증") {
-            userRepository.findByUserInformationEmail("kitty@gmail.com") shouldBe null
-        }
+        context("한 개의 유저 데이터가 존재하는 경우에 대한") {
+            val expected = userRepository.save(User.create("kitty", "kitty@gmail.com", "pass", "token"))
 
-        it("엔터티 영속 확인") {
-            val result = userRepository.save(User.create("kitty", "kitty@gmail.com", "pass", "token"))
+            it("findByEmail 쿼리 검증") {
+                val actual = userRepository.findByUserInformationEmail("kitty@gmail.com")
 
-            assertSoftly(result) {
-                userId shouldNotBe null
-                username shouldBe "kitty"
-                email shouldBe "kitty@gmail.com"
-                createdAt shouldNotBe null
-                updatedAt shouldNotBe null
-                verified shouldBe false
+                expected shouldBe actual
+                expected.userId shouldBe actual?.userId
+                expected.username shouldBe actual?.username
+                expected.email shouldBe actual?.email
+                expected.createdAt shouldBe actual?.createdAt
+                expected.updatedAt shouldBe actual?.updatedAt
+                expected.verified shouldBe actual?.verified
             }
         }
     }
