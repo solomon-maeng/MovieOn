ce36333cc9ceb678d2ccb13f625a59d04f2d4f60
solomon.maeng <solomon.maeng@dramancompany.com>
2022-12-14 17:03:47 +0900

    Add empty error response

--- user-api/user-interfaces/src/main/kotlin/com/remember/user/interfaces/api.kt
+++ user-api/user-interfaces/src/main/kotlin/com/remember/user/interfaces/api.kt
@@ -5,6 +5,7 @@ import com.remember.user.application.TokenDto
 import com.remember.user.application.UserDto
 import com.remember.user.application.UserFacade
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirements
@@ -24,7 +25,7 @@ interface UserApiSpecification {
     @Operation(summary = "회원가입", description = "유저가 회원가입 하기 위한 EndPoint 를 제공합니다.")
     @ApiResponses(
         value = [ApiResponse(responseCode = "200", description = "회원가입 성공"),
-            ApiResponse(responseCode = "400", description = "회원가입 요청 파라미터 에러")]
+            ApiResponse(responseCode = "400", description = "회원가입 요청 파라미터 에러", content = [Content()])]
     )
     @PostMapping("/api/v1/users/register")
     fun register(@ParameterObject @RequestBody @Valid request: RegisterUserRequest): ResponseEntity<UserResponse>
@@ -32,7 +33,7 @@ interface UserApiSpecification {
     @Operation(summary = "회원가입 확인", description = "회원가입한 유저가 올바른지 확인 하기 위한 EndPoint 를 제공합니다.")
     @ApiResponses(
         value = [ApiResponse(responseCode = "303", description = "회원가입 처리 완료 및 리다이렉트"),
-            ApiResponse(responseCode = "400", description = "회원가입 확인 파라미터 에러")]
+            ApiResponse(responseCode = "400", description = "회원가입 확인 파라미터 에러", content = [Content()])]
     )
     @GetMapping(value = ["/api/v1/users/register/confirm"])
     fun registerConfirm(@ParameterObject @ModelAttribute @Valid request: RegisterConfirmRequest): ResponseEntity<Any>
@@ -40,7 +41,7 @@ interface UserApiSpecification {
     @Operation(summary = "로그인", description = "회원가입 완료 사용자가 로그인하는 EndPoint 를 제공합니다.")
     @ApiResponses(
         value = [ApiResponse(responseCode = "200", description = "로그인 성공"),
-            ApiResponse(responseCode = "400", description = "로그인 요청 파라미터 에러")]
+            ApiResponse(responseCode = "400", description = "로그인 요청 파라미터 에러", content = [Content()])]
     )
     @PostMapping("/api/v1/users/login")
     fun login(@ParameterObject @RequestBody @Valid request: LoginUserRequest): ResponseEntity<TokenResponse>
@@ -48,7 +49,7 @@ interface UserApiSpecification {
     @Operation(summary = "토큰 재발급", description = "토큰을 재발급하는 EndPoint 를 제공합니다.")
     @ApiResponses(
         value = [ApiResponse(responseCode = "200", description = "토큰 재발급 성공"),
-            ApiResponse(responseCode = "400", description = "토큰 재발급 요청 에러")]
+            ApiResponse(responseCode = "400", description = "토큰 재발급 요청 에러", content = [Content()])]
     )
     @PostMapping("/api/v1/users/reIssuance")
     fun reIssuance(@RequestHeader("Authorization") payload: String): ResponseEntity<TokenResponse>
